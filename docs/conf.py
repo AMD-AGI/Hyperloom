f6ff8e7bb48f79400b8b20c4bd0ab920f50e6869
Matt Williams <Matt.Williams+amdeng@amd.com>
2026-07-07 07:16:46 -0400

    Header links

--- docs/conf.py
+++ docs/conf.py
@@ -35,13 +35,13 @@ html_theme_options = {
     "header_link": False,
     "version_list_link": False,
     "nav_secondary_items": {
-        "GitHub": False,
+        "GitHub": "https://github.com/AMD-AGI/Hyperloom",
         "Community": False,
         "Blogs": "https://rocm.blogs.amd.com/",
         "ROCm Developer Hub": "https://www.amd.com/en/developer/resources/rocm-hub.html",
         "Instinct™ Docs": "https://instinct.docs.amd.com/",
         "Infinity Hub": "https://www.amd.com/en/developer/resources/infinity-hub.html",
-        "Support": False,
+        "Support": "https://github.com/AMD-AGI/Hyperloom/issues/new/choose",
     },
     "link_main_doc": False,
 }
